16232cad2d3426ec9e6976ae460d7beb76b93850
Robin Gustafsson <robin@rgson.se>
2020-01-24 21:57:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# [vis2k/Mirror] for the Unity Package Manager
+
+The Unity Package Manager is in many ways a more convenient alternative to the
+Unity Asset Store for dependency management. Unfortunately, the maintainer of
+[vis2k/Mirror] has [decided against UPM support][vis2k-reason] to drive more
+traffic to the Asset Store.
+
+This repository adds UPM support to the upstream version of Mirror. It is not a
+fork of Mirror, but rather an otherwise unmodified repackaging of the upstream
+to support installation through the Unity Package Manager.
+
+Inspired by this [issue][vis2k/Mirror#891] and [pull request][vis2k/Mirror#892].
+
+<!-- Refs -->
+[vis2k/Mirror]: https://github.com/vis2k/Mirror
+[vis2k/Mirror#891]: https://github.com/vis2k/Mirror/issues/891
+[vis2k/Mirror#892]: https://github.com/vis2k/Mirror/pull/892
+[vis2k-reason]: https://github.com/vis2k/Mirror/pull/892#issuecomment-544277802
---
+++ import.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+set -e
+
+upstream_repo='git@github.com:vis2k/Mirror.git'
+
+# Read the upstream repo
+git remote add upstream "$upstream_repo" >/dev/null 2>&1 || true
+git fetch upstream
+
+# Find the latest version
+latest=$(git tag --list 'v*' | sort -Vr | head -n1)
+if git rev-parse "upm/$latest" >/dev/null 2>&1; then
+	echo "Already on newest version ($latest)"
+	exit 0
+fi
+
+# Import the new version
+rm -rf Mirror
+mkdir Mirror
+git archive "$latest" Assets/Mirror | tar --strip-components=2 -C Mirror -x
+
+# Generate package.json
+cat > Mirror/package.json <<JSON
+{
+	"name": "com.vis2k.mirror",
+	"displayName": "Mirror",
+	"version": "${latest#v}",
+	"unity": "2019.1",
+	"description": "Mirror is a high level Networking API for Unity, supporting different low level Transports.",
+	"author": "vis2k",
+	"repository": {
+		"type":"git",
+		"url":"https://github.com/rgson/Mirror-UPM.git"
+	},
+	"samples":[
+JSON
+first=yes
+for example_d in Mirror/Examples/*/; do
+	if [ $first = 'no' ]; then echo '		,' >> Mirror/package.json; fi
+	first=no
+	example=$(basename "$example_d")
+	cat >> Mirror/package.json <<JSON
+		{
+			"displayName": "$example",
+			"description": "$example",
+			"path": "Examples/$example"
+		}
+JSON
+done
+cat >> Mirror/package.json <<JSON
+	]
+}
+JSON
+
+# Commit the new version
+git add Mirror
+git commit -m "Version $latest"
+
+git branch -D upm >/dev/null 2>&1 || true
+git subtree split -P Mirror -b upm
+
+git tag "upm/$latest" upm
+
+if [ "$1" = 'push' ]; then
+	git push origin master:master
+	git push -f origin upm:upm
+	git push origin upm/$latest
+fi
